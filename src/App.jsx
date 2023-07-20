28265fd0ae4ad329fe57e795e30c715b070484d6
whuang214 <whuang2146@gmail.com>
2023-07-20 15:13:10 -0400

    add search states

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import GiphySearchForm from "./GiphySearchForm/GiphySearchForm.jsx";
 
 export default function App() {
-  const API_KEY = import.meta.env.VITE_GIPHY_API_KEY;
+  const [searchPhrase, setSearchPhrase] = useState("");
+  const [gif, setGif] = useState(null); // one gif
+
+  useEffect(() => {
+    if (!searchPhrase) return;
+
+    const API_KEY = import.meta.env.VITE_GIPHY_API_KEY;
+    const searchURL = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${searchPhrase}&limit=1`;
+
+    console.log(searchURL);
+
+    async function fetchGifs() {
+      try {
+        const response = await fetch(searchURL);
+        const json = await response.json();
+        console.log(json);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }, [searchPhrase]);
+
+  function handleSearchPhraseChange(newSearchPhrase) {
+    setSearchPhrase(newSearchPhrase);
+  }
 
   return (
-    <>
-      <GiphySearchForm />
-    </>
+    <div className="search-container">
+      <h1>Giphy Search</h1>
+      <GiphySearchForm onSearchPhraseChange={handleSearchPhraseChange} />
+    </div>
   );
 }
--- src/GiphySearchForm/GiphySearchForm.jsx
+++ src/GiphySearchForm/GiphySearchForm.jsx
@@ -1,8 +1,23 @@
-export default function GiphySearchForm() {
+import { useState } from "react";
+
+export default function GiphySearchForm({ onSearchPhraseChange }) {
+  const [searchPhrase, setSearchPhrase] = useState("");
+
+  function handleChange(event) {
+    setSearchPhrase(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    onSearchPhraseChange(searchPhrase);
+    // console.log("submitting", searchPhrase);
+    setSearchPhrase("");
+  }
+
   return (
     <form>
-      <input type="text" />
-      <button>Search</button>
+      <input type="text" onChange={handleChange} />
+      <button onClick={handleSubmit}>Search</button>
     </form>
   );
 }
